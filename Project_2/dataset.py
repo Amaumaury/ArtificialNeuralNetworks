dd4e884dc4645d034f344de42f44aeeaab6ef160
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2018-04-30 11:12:06 +0200

    Add type annotations

--- Project_2/dataset.py
+++ Project_2/dataset.py
@@ -3,13 +3,15 @@ import numpy as np
 import music21
 from tqdm import tqdm_notebook
 
+from typing import List, Dict, Callable, Set, Tuple
+
 datapath = "chorpus/"
 
 class Dataset:
 
-    def __init__(self, data, build_matrix_representation=False):
+    def __init__(self, data: Dict[str, Dict[str, List[int]]], build_matrix_representation=False) -> 'Dataset':
         self.melodies = [Melody(melody_name, representation) for melody_name, representation in list(data.items())]
-        self.id_to_pitches, pitches_to_id = self.__build_mappings(self.extract_piches())
+        self.id_to_pitches, pitches_to_id = self.__build_mappings(self.extract_pitches())
         self.id_to_durations, durations_to_id = self.__build_mappings(self.extract_durations())
 
         if build_matrix_representation:
@@ -17,62 +19,62 @@ class Dataset:
             for melody in self.melodies:
                 melody.build_integer_representation(self.id_to_pitches, pitches_to_id, self.id_to_durations, durations_to_id)
                 melody.build_matrix_representation(len(self.id_to_pitches), len(self.id_to_durations))
-                max_length = max(max_length, melody.get_length())
+                max_length = max(max_length, len(melody))
 
             for melody in self.melodies:
                 melody.build_standardized_matrix_representation(max_length)
 
-    def get_mappings(self):
+    def get_mappings(self) -> Dict[str, Dict[int, int]]:
         return {'P': self.id_to_pitches, 'T': self.id_to_durations}
 
     def with_matrix_representation(self):
         return Dataset(self.get_midi_representation(), True)
 
-    def filter(self, condition):
-        filtered_melodies = [melody.get_midi_representation() for melody in self.melodies if condition(melody)]
-        return Dataset({name: representation for name, representation in filtered_melodies})
+    def filter(self, condition: Callable[[Melody], bool]) -> 'Dataset':
+        melodies = map(lambda melody: melody.get_midi_representation(), filter(condition, self.melodies))
+        return Dataset({name: representation for name, representation in melodies})
 
-    def apply_to_melody(self, map_):
+    def apply_to_melody(self, map_: Callable[[Melody], Melody]) -> 'Dataset':
         mapped_melodies = [map_(melody).get_midi_representation() for melody in self.melodies]
         return Dataset({name: representation for name, representation in mapped_melodies})
 
-    def delete_by_name(self, melody_name):
+    def delete_by_name(self, melody_name: str) -> 'Dataset':
         return self.filter(lambda melody: melody.get_name() != melody_name)
 
-    def get_number_of_melodies(self):
+    def __len__(self) -> int:
         return len(self.melodies)
 
-    def get_n_random_melodies(self, n, seed=0):
+    def get_n_random_melodies(self, n: int, seed=0) -> List[Melody]:
         np.random.seed(seed)
         return [melody for melody in np.random.choice(self.melodies, n)]
 
-    def get_midi_representation(self):
+    def get_midi_representation(self) -> Dict[str, Dict[str, List[int]]]:
         melodies = [melody.get_midi_representation() for melody in self.melodies]
         return {name: representation for name, representation in melodies}
 
     def get_integer_representation(self):
+        # TODO: Fix
         melodies = [melody.get_integer_representation() for melody in self.melodies]
         return {name: representation for name, representation in melodies}
+    
+    def extract_pitches(self) -> Set[int]:
+        return set().union(*[melody.extract_pitches() for melody in self.melodies])
 
-    def get_all_midi_durations(self):
+    def get_all_midi_durations(self) -> List[int]:
         flatten = lambda l: [item for sublist in l for item in sublist]
         return flatten([melody.get_midi_durations() for melody in self.melodies])
 
-    def extract_piches(self):
-        return set().union(*[melody.extract_piches() for melody in self.melodies])
-
-    def extract_durations(self):
+    def extract_durations(self) -> Set[int]:
         return set().union(*[melody.extract_durations() for melody in self.melodies])
 
-    def __build_mappings(self, possible_values):
+    def __build_mappings(self, possible_values: Set[int]) -> Tuple[Dict[int, int], Dict[int, int]]:
         inv_map = lambda dict_: {v: k for k, v in dict_.items()}
         ids = range(len(possible_values))
         id_to_value = dict(zip(ids, sorted(possible_values)))
         return id_to_value, inv_map(id_to_value)
 
-    def transposeDataset(self):
+    def transposeDataset(self) -> 'Dataset':
         transposed_dataset = {}
-
         dataset = self.get_midi_representation()
 
         for label in tqdm_notebook(list(dataset.keys())):
--- Project_2/melody.py
+++ Project_2/melody.py
@@ -1,9 +1,10 @@
 from keras.preprocessing.sequence import pad_sequences
 import keras.utils
 
+from typing import List, Dict, Set, Tuple, Iterable
 class Melody:
 
-    def __init__(self, name, representation):
+    def __init__(self, name: str, representation: Dict['str', List[int]]) -> 'Melody':
         self.name = name
         self.midi_pitches = representation['P']
         self.midi_durations = representation['T']
@@ -12,44 +13,46 @@ class Melody:
         self.matrix_pitches = None
         self.matrix_durations = None
 
-    def get_length(self):
+    def __len__(self) -> int:
         return len(self.midi_pitches)
 
-    def get_name(self):
+    def get_name(self) -> str:
         return self.name
 
-    def get_integer_representation(self):
+    def get_integer_representation(self) -> Tuple[str, Dict[str, List[int]]]:
         return (self.name, {'P': self.integer_pitches, 'T': self.integer_durations})
 
-    def get_midi_representation(self):
+    def get_midi_representation(self) -> Tuple[str, Dict[str, List[int]]]:
         return (self.name, {'P': self.midi_pitches, 'T': self.midi_durations})
 
     def get_matrix_representation(self):
+        # TODO: Add type
         return (self.name, {'P': self.matrix_pitches, 'T': self.matrix_durations})
 
     def get_std_matrix_representation(self):
         return (self.name, {'P': self.std_matrix_pitches, 'T': self.std_matrix_durations})
 
-    def get_midi_durations(self):
+    def get_midi_durations(self) -> List[int]:
         return self.midi_durations
 
-    def extract_piches(self):
+    def extract_pitches(self) -> List[int]:
         return set(self.midi_pitches)
 
-    def extract_durations(self):
+    def extract_durations(self) -> Set[int]:
         return set(self.midi_durations)
 
-    def intersect_midi_durations(self, test_set):
+    def intersect_midi_durations(self, test_set: Iterable[int]) -> Set[int]:
         return set(test_set).intersection(set(self.midi_durations))
 
-    def build_integer_representation(self, id_to_pitches, pitches_to_id, id_to_durations, durations_to_id):
+    def build_integer_representation(self, id_to_pitches: Dict[int, int], pitches_to_id: Dict[int, int],
+         id_to_durations: Dict[int, int], durations_to_id: Dict[int, int]):
         self.integer_pitches = [pitches_to_id[midi_pitch] for midi_pitch in self.midi_pitches]
         self.integer_durations = [durations_to_id[midi_duration] for midi_duration in self.midi_durations]
 
-    def build_matrix_representation(self, number_of_pitches, number_of_durations):
+    def build_matrix_representation(self, number_of_pitches: int, number_of_durations: int):
         self.matrix_pitches = keras.utils.to_categorical(self.integer_pitches, number_of_pitches)
         self.matrix_durations = keras.utils.to_categorical(self.integer_durations, number_of_durations)
 
-    def build_standardized_matrix_representation(self, max_length):
+    def build_standardized_matrix_representation(self, max_length: int):
         self.std_matrix_pitches = pad_sequences(self.matrix_pitches.T, maxlen=max_length).T
         self.std_matrix_durations = pad_sequences(self.matrix_durations.T, maxlen=max_length).T
