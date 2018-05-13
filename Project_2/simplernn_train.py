86d9e8c134c361608fd3f9baea3c08b235ec4614
amaumaury <amaurycombes@hotmail.fr>
2018-05-13 11:29:07 +0200

    Add early stopping to model training

--- Project_2/simplernn_train.py
+++ Project_2/simplernn_train.py
@@ -9,6 +9,26 @@ from keras.preprocessing.sequence import pad_sequences
 from keras.callbacks import ModelCheckpoint
 
 
+class RealEarlyStopper(keras.callbacks.Callback):
+    def __init__(self, set_best_at_end=True):
+        self._best_score = -1
+        self._best_weights = None
+        self._set_best_at_end = set_best_at_end
+
+    def on_epoch_end(self, epoch=None, logs={}):
+        valacc = logs['val_acc']
+        if valacc > self._best_score:
+            self._best_score = valacc
+            self._best_weights = [np.copy(layer.get_weights()) for layer in self.model.layers]
+
+    def on_train_end(self, logs={}):
+        if not self._set_best_at_end:
+            return
+        print('Restoring the model parameters giving best validation accuracy: {}'.format(self._best_score))
+        for layer, best_weights in zip(self.model.layers, self._best_weights):
+            layer.set_weights(best_weights)
+
+
 # Load data
 final_dataset = Dataset.load_from_file('final_dataset')
 max_melody_length = final_dataset.get_max_melody_len()
@@ -31,15 +51,15 @@ def buildModel(dictionaries, batch_length, dropout=0.2, activation='GRU', Hsize=
     H = dict()
     M = dict()
     Y = dict()
-    
+
     X['T'] = Input(shape=(batch_length, len(dictionaries['T'])), name="XT")
     X['P'] = Input(shape=(batch_length, len(dictionaries['P'])), name="XP")
-    
+
     M['T'] = Masking(mask_value=0., name="MT")(X['T'])
     M['P'] = Masking(mask_value=0., name="MP")(X['P'])
-    
+
     H['1'] = Concatenate(name="MergeX")([M['T'], M['P']])
-    
+
     if activation == 'GRU':
         rnn_layer = GRU(Hsize, dropout=dropout, return_sequences=True)(H['1'])
     elif activation == 'LSTM':
@@ -47,24 +67,24 @@ def buildModel(dictionaries, batch_length, dropout=0.2, activation='GRU', Hsize=
         #Your hidden layer(s) architecture with LSTM (For your own curiosity, not required for the project)
     elif activation == 'RNN':
         rnn_layer = SimpleRNN(Hsize, dropout=dropout, return_sequences=True)(H['1'])
-        
+
     print(rnn_layer)
 
     Y['T'] = TimeDistributed(Dense(len(dictionaries['T']), activation='softmax'), name='YT')(rnn_layer)
     Y['P'] = TimeDistributed(Dense(len(dictionaries['P']), activation='softmax'), name='YP')(rnn_layer)
-    
-    model = Model(inputs = [X['T'], X['P']], outputs = [Y['T'], Y['P']])
-    opt = Adam() 
+
+    model = Model(inputs=[X['T'], X['P']], outputs=[Y['T'], Y['P']])
+    opt = Adam()
     model.compile(
-        loss='categorical_crossentropy', 
+        loss='categorical_crossentropy',
         optimizer=opt,
         metrics=['accuracy'])
-    
-    
+
+
     return model
 
 # Create model
-RNNmodel = buildModel(hack_dict, 
+RNNmodel = buildModel(hack_dict,
                       batch_length=max_melody_length-1,#Put here the number of notes (timesteps) you have in your Zero-padded matrices
                       activation='GRU')
 
@@ -75,7 +95,8 @@ history = RNNmodel.fit(
     x=[x_durations_training_array, x_pitches_training_array],
     y=[y_durations_training_array, y_pitches_training_array],
     epochs=250,
-    validation_split=0.2
+    validation_split=0.2,
+    callbacks=[RealEarlyStopper()]
 )
 
 # Save
